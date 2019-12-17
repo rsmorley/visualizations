ea7715283294d5dc0ee24cc8756c7aacdd9d5405
Scott Morley <smorley@sofi.com>
2019-12-17 12:49:47 -0700

    Added vertical menu and removed action buttons.

--- js/src/index.js
+++ js/src/index.js
@@ -21,8 +21,6 @@ function component() {
   let svg = document.getElementById('visualization-canvas');
   let {width, height} = getCanvasDimensions(svg);
 
-  mainDiv.appendChild(createButtons());
-  
   drawWalkingCirclesWithVaryingDiameters(svgContainer, width/2, height/2, 0, 1);
 }
 
@@ -38,94 +36,87 @@ function createMenu() {
   menuHeading1.classList.add('pure-menu-heading');
   menuDiv.appendChild(menuHeading1);
 
-  const menuList1 = document.createElement('ul');
-  menuList1.classList.add('pure-menu-list');
-
-  const menuItem1 = document.createElement('li');
-  menuItem1.classList.add('pure-menu-item');
-
-  const menuLink1 = document.createElement('a');
-  menuLink1.innerHTML = 'Circle Walk';
-  menuLink1.classList.add('pure-menu-link');
-
-  menuItem1.appendChild(menuLink1);
-  menuList1.appendChild(menuItem1);
-  menuDiv.appendChild(menuList1);
+  const visMenuItems = new Array();
+  visMenuItems.push(
+    {
+      listItemClasses: new Array('pure-menu-selected'),
+      id: 'menu-circle-link',
+      text: 'Circle Walk'
+    });
+  menuDiv.appendChild(createMenuItems(visMenuItems));
 
   const menuHeading2 = document.createElement('span');
   menuHeading2.innerHTML = 'Actions';
   menuHeading2.classList.add('pure-menu-heading');
   menuDiv.appendChild(menuHeading2);
 
-  const menuList2 = document.createElement('ul');
-  menuList2.classList.add('pure-menu-list');
+  const actionMenuItems = new Array();
+  actionMenuItems.push(
+    {
+      listItemClasses: new Array('menu-item-divided'),
+      click: printSvg,
+      id: 'menu-print-link',
+      text: 'Take Snapshot'
+    });
+  actionMenuItems.push(
+    {
+      click: clearCanvas,
+      id: 'menu-clear-link',
+      text: 'Clear Canvas'
+    });
+  actionMenuItems.push(
+    {
+      linkClasses: new Array('pure-menu-disabled'),
+      id: 'menu-download-link',
+      text: 'Download Snapshot'
+    });
 
-  const menuItem2 = document.createElement('li');
-  menuItem2.classList.add('pure-menu-item');
+  menuDiv.appendChild(createMenuItems(actionMenuItems));
   
-  const menuLink2 = document.createElement('a');
-  menuLink2.innerHTML = 'Take Snapshot';
-  menuLink2.classList.add('pure-menu-link');
-
-  menuItem2.appendChild(menuLink2);
-  menuList2.appendChild(menuItem2);
-  menuDiv.appendChild(menuList2);
-
   menuContainer.appendChild(menuDiv);
-
   return menuContainer;
 }
 
-function createButtons() {
-  // create buttons
-  const mainDiv = document.createElement('div');
-  mainDiv.classList.add('pure-g');
+/**
+ * 
+ * @param {Array} items array of objects
+ *  text: text displayed for menu item - required 
+ *  click: onclick function - optional
+ *  linkClasses: array of classes to add to href - optional
+ *  listItemClasses: array of classes to add to li- optional
+ */
+function createMenuItems (items) {
 
-  const leftDiv = document.createElement('div');
-  leftDiv.classList.add('pure-u-1');
-  leftDiv.classList.add('pure-u-md-1-2');
-  leftDiv.classList.add('pure-u-xl-1-3');
-  leftDiv.classList.add('center-aligned-div');
-  const rightDiv = document.createElement('div');
-  rightDiv.classList.add('pure-u-1');
-  rightDiv.classList.add('pure-u-md-1-2');
-  rightDiv.classList.add('pure-u-xl-2-3');
-  rightDiv.classList.add('center-aligned-div');
-  mainDiv.appendChild(leftDiv);
-  mainDiv.appendChild(rightDiv);
+  const menuList = document.createElement('ul');
+  menuList.classList.add('pure-menu-list');
 
-  const saveBtn = document.createElement('button');
-  saveBtn.innerHTML = 'Take a snapshot';
-  saveBtn.onclick = printSvg;
-  saveBtn.classList.add('pure-button');
-  saveBtn.classList.add('pure-button-primary');
-  saveBtn.classList.add('button-rounded');
-  leftDiv.appendChild(saveBtn);
+  _.forEach(items, (item) => {
+    const listItem = document.createElement('li');
+    listItem.classList.add('pure-menu-item');
+    if (_.isArray(item.listItemClasses)) {
+      _.forEach(item.listItemClasses, (className) => {
+        listItem.classList.add(className);
+      });
+    }
 
-  // create a button with a link wrapper to we can download the svg file
-  const downloadLink = document.createElement('a');
-  downloadLink.id = 'download-link';
-  downloadLink.title = 'Download SVG';
-  const downloadBtn = document.createElement('button');
-  downloadBtn.id = 'download-button';
-  downloadBtn.innerHTML = 'Download Snapshot';
-  downloadBtn.classList.add('pure-button');
-  downloadBtn.classList.add('pure-button-primary');
-  downloadBtn.classList.add('pure-button-disabled');
-  downloadBtn.classList.add('button-rounded');
-  downloadLink.appendChild(downloadBtn);
-  leftDiv.appendChild(downloadLink);
+    const menuLink = document.createElement('a');
+    menuLink.classList.add('pure-menu-link');
+    menuLink.innerHTML = item.text;
+    if (_.isFunction(item.click)) {
+      menuLink.onclick = item.click;
+    }
+    if (_.isArray(item.linkClasses)) {
+      _.forEach(item.linkClasses, (className) => {
+        menuLink.classList.add(className);
+      });
+    }
+    menuLink.id = item.id;
 
-  const clearBtn = document.createElement('button');
-  clearBtn.innerHTML = 'Clear Canvas';
-  clearBtn.id = 'clear-button';
-  clearBtn.onclick = clearCanvas;
-  clearBtn.classList.add('pure-button');
-  clearBtn.classList.add('pure-button-primary');
-  clearBtn.classList.add('button-rounded');
-  leftDiv.appendChild(clearBtn);
+    listItem.appendChild(menuLink);
+    menuList.appendChild(listItem);
+  });
 
-  return mainDiv;
+  return menuList;
 }
 
 function drawWalkingCirclesWithVaryingDiameters(svgContainer, xCoord, yCoord, colorIndex, frameCount) {
--- js/src/print.js
+++ js/src/print.js
@@ -4,9 +4,8 @@ export default function printSvg() {
   let source = serializer.serializeToString(canvas);
 
   source = '<?xml version="1.0" standalone="no"?>\r\n' + source;
-  let downloadLink = document.getElementById('download-link');
-  let downloadBtn = document.getElementById('download-button');
+  let downloadLink = document.getElementById('menu-download-link');
   downloadLink.href = 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(source);
-  downloadBtn.classList.remove('pure-button-disabled');
+  downloadLink.classList.remove('pure-menu-disabled');
   downloadLink.download = "visual.svg";
 }
